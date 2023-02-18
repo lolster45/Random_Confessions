c21183d4f1d9e1ffe8f78a468322b2e4ac11d1de
lolster45 <oscarneutron3@gmail.com>
2023-02-17 18:53:55 -0600

    fixed

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -3,20 +3,14 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+  apiKey: "AIzaSyCjs0z64ysfBUYgs9Lpub8fdi8HNF6WNxU",
+  authDomain: "confessions-website.firebaseapp.com",
+  projectId: "confessions-website",
+  storageBucket: "confessions-website.appspot.com",
+  messagingSenderId: "597392200228",
+  appId: "1:597392200228:web:675815f5adaf6f595b3eb1",
+  measurementId: "G-XQLCKPNL9Y"
 };
-
-
-
-
-
-
 
 
 // Initialize Firebase
